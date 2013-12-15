4dd6eed52512d315a64e611c78db29ddd29d5d1b
U-acomputernamel\ciaran <ciaran@acomputernamel.(none)>
2013-12-15 20:11:30 +0000

    Same scope in directive and controller

--- public/js/ng-map.js
+++ public/js/ng-map.js
@@ -1,71 +1,46 @@
 var app = angular.module("Mapp", []);
 
-
 app.controller("markerCtrl", ["$scope", "$http", function($scope, $http) {
-	$scope.event = {
-		
-	},
 
-	$scope.save = function() {
-		alert("SAVE");
-		console.log($scope.event);
+	$scope.id = 0,
+	$scope.markers = [],
 
-		/*$http.post('/collections/markers', {text: $scope.inputText})
-        	.success(function(data, status, headers, config) {
-            	console.log("posted successfully");
-            	//$scope.messages.unshift(data);
-            	//$scope.inputText = "";
-        });*/
-	}, 
 	$scope.shout = function() {
 		alert("HI!");
+		console.log("controller",$scope);
 	}
-
 }]);
 
-app.directive('map', function($compile) {
+app.directive('map', function() {
 	return {
 		restrict: 'E',
-		replace: true,
-        template: '<div></div>',
-  
-
+		replace:true,
+		template:"<div></div>",
 		link: function(scope, element, attrs) {
-	    	var mapOptions = {    
+			console.log("directive",scope);
+			var mapOptions = {    
 	          zoom: 12,
 	          center: new google.maps.LatLng(53.349578,-6.260258),
 	          mapTypeId: google.maps.MapTypeId.ROADMAP
 	        };
     
     		var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-
-    		var html = 
-    		"<form id=\"markerInput\" ng-controller=\"markerCtrl\" ng-submit=\"save()\">"+
-    		"<div class=\"form-group\"><label for=\"title\">Title</label><input ng-model=\"event.title\" class=\"form-control\" placeholder=\"Title\"></div>"+ 
-    		"<div class=\"form-group\"><label for=\"desc\">Desc</label><input ng-model=\"event.desc\" class=\"form-control\" placeholder=\"Description\"></div>"+ 
-    		"<div class=\"form-group\"><label for=\"start\">Start</label><input ng-model=\"event.start\" class=\"form-control\" placeholder=\"Start\"></div>"+ 
-    		"<div class=\"form-group\"><label for=\"end\">End</label><input ng-model=\"event.end\" class=\"form-control\" placeholder=\"End\"></div>"+ 
-    		"<div class=\"form-group\"><label for=\"tags\">Tags</label><input ng-model=\"event.tags\" class=\"form-control\" placeholder=\"Tags\"></div>"+
-    		"<div class=\"form-group\"><button type=\"submit\" class=\"btn btn-default\">Submit</button>"+
-    		"</form>";
-    		
-    		compiled = $compile(html)(scope);
-
-		    infowindow = new google.maps.InfoWindow();
-
-		    google.maps.event.addListener(map, "click", function(event) {
-		        marker = new google.maps.Marker({
+			
+			google.maps.event.addListener(map, "click", function(event) {
+				scope.shout();
+		        var marker = new google.maps.Marker({
 		          position: event.latLng,
-		          map: map
+		          map: map,
+		          id: scope.id++
 		        });
-		    	google.maps.event.addListener(marker, "click", function() {
-		    		infowindow.setContent(compiled[0]);
-		    		infowindow.open(map, marker);
-		    	});
+
+		        scope.markers.push(marker);
+
+
 		    });
-			    
-    
-    
+
+    		
 		}
 	}
+			
 });
