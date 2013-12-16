b4d9d8ceb58c166fe842099c2da3300a90f60dd3
U-acomputernamel\ciaran <ciaran@acomputernamel.(none)>
2013-12-16 00:14:10 +0000

    Working ok

--- public/js/ng-map.js
+++ public/js/ng-map.js
@@ -2,7 +2,12 @@ var app = angular.module("Mapp", []);
 
 app.controller("markerCtrl", ["$scope", "$http", function($scope, $http) {
 
-	$scope.id = 0,
+	$scope.id = 0;
+	$scope.markerEvent = {
+		lat: 0,
+		lng: 0,
+		title: ""
+	}
 	$scope.markers = [],
 
 	$scope.shout = function() {
@@ -27,14 +32,20 @@ app.directive('map', function() {
     		var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 			
 			google.maps.event.addListener(map, "click", function(event) {
-				scope.shout();
+				//scope.shout();
 		        var marker = new google.maps.Marker({
 		          position: event.latLng,
 		          map: map,
-		          id: scope.id++
 		        });
 
-		        scope.markers.push(marker);
+
+		        scope.markers.push({
+		        	id: scope.id++,
+		        	pos: event.latLng
+		        });
+
+		        console.log(scope.markers);
+
 
 
 		    });
@@ -44,3 +55,15 @@ app.directive('map', function() {
 	}
 			
 });
+
+/*
+app.directive('markers', function() {
+	return {
+		restrict: 'E',
+		template: '<ul><li ng-repeat=\"marker in markers\">{{marker.id}}: {{marker.position}}</li></ul>',
+		link: function(scope, element, attrs) {
+
+		}
+	}
+
+});*/
